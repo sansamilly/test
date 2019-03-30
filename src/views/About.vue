6b5088e337e6de6fc6ec7405fc3386a360223e29
sansamilly <sansamilly_w@163.com>
2019-03-30 22:03:25 +0800

    first

---
+++ src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h1>{{count}}</h1>
+  </div>
+</template>
+<script>
+import store from "@/store";
+export default {
+  name: "about",
+  data: () => {
+    return {
+      count: store.state.count
+    }
+  }
+}
+</script>
+>
---
+++ src/views/Test.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <div>{{msg}}</div>
+      <button type="button" @click="add()">飞天吧</button>
+  </div>
+</template>
+<script>
+import store from '@/store';
+export default {
+  name: "test",
+  data () {
+    return {
+      msg: "Hello, this is test page."
+    }
+  },
+  methods: {
+    add: () => {
+      store.commit("flying");
+    }
+  }
+}
+</script>
+
